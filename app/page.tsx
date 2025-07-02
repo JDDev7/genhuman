619d5d57b3b2503e1353db7b6bfb2814ff5495bd
Jose Diego Moreno <jdmorenopulido@gmail.com>
2025-07-02 11:05:00 +0200

    implementado scroll suave con Lenis

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,28 @@ export default function Home() {
         <h3>Más información</h3>
       </div>
     </div>
+        <div className={styles.HeroSection}>
+      <div className={styles.HeroSectionContent}>
+      <h1>¡Bienvenido a GenHuman!</h1>
+      <h2>Ponle cara a tus personajes de rol de mano de nuestros artistas gracias a nuestro creador de personajes</h2>
+      <button className={`${montserrat.className}`}><Link className={styles.ButtonLink} href="/charcreator">¡Pruébalo!</Link></button>
+      </div>
+      <div className={styles.More}>
+        <ArrowDownIcon className={styles.ArrowIcon} />
+        <h3>Más información</h3>
+      </div>
+    </div>
+        <div className={styles.HeroSection}>
+      <div className={styles.HeroSectionContent}>
+      <h1>¡Bienvenido a GenHuman!</h1>
+      <h2>Ponle cara a tus personajes de rol de mano de nuestros artistas gracias a nuestro creador de personajes</h2>
+      <button className={`${montserrat.className}`}><Link className={styles.ButtonLink} href="/charcreator">¡Pruébalo!</Link></button>
+      </div>
+      <div className={styles.More}>
+        <ArrowDownIcon className={styles.ArrowIcon} />
+        <h3>Más información</h3>
+      </div>
+    </div>
   </section>
   );
 }
